91f89c8eeeac3780698c0aa90314cd0e91e06392
muraliJ2k <murali.j2k@gmail.com>
2017-07-07 13:18:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-
-
 import os
 
 from flask import Flask
 from flask import request
 from flask import make_response
-import requests
 import logging
 import sys
+from zeep import Client
+from datetime import datetime
+import json
 
 # Flask app should start in global layout
 app = Flask(__name__)
@@ -16,15 +16,29 @@ app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
 
-
-
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    if request.headers['apikey'] == 'a3be1e29-8d95-474c-9ae8-faa88ade48b4':
-        response = requests.get('http://noecommercews1098.cloudapp.net/api.ai/webhook.ashx', data = request.data)
-        return response.text
-    else:
-        return 'foo'
+    chatresponse = {}
+    try:
+        #get the fulfilnment request
+        fulfilmentrequest = request.data
+        fulfilmentrequest = json.loads(fulfilmentrequest.decode('UTF-8'))
+        client = Client('https://apiaiwebhookpassthough.herokuapp.com/static/content/EntitlementService.xml')    
+        requesttype = client.get_element('ns2:EntitlementRequest')
+        entitlementrequest = requesttype(BusinessOrgID = 1, ProductTypeCode = 'P', RequestedByUserID = '26may2017', SerialNumber = fulfilmentrequest['result']['parameters']['slno'], SymptomIDList = '0707')
+        response = client.service.Entitle(request = entitlementrequest)
+        warrantyexpirydate = response['Entitlement']['WarrantyExpiryDate']   
+        if datetime.now() < warrantyexpirydate:
+            chatresponse['displayText'] = "The phone is under warranty"
+            chatresponse['speech'] = "Your phone is under warranty"
+        else:
+            chatresponse['displayText'] = "The phone is not under warranty"
+            chatresponse['speech'] = "Your phone is not under warranty"
+    except:
+        chatresponse['displayText'] = "Unable to find warranty details!  Please check the serial number and try again"
+        chatresponse['speech'] = "Check the serial number and try again"
+
+    return json.dumps(chatresponse)
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
